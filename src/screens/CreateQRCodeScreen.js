7db6531a1375fab6bff91f9445d318624eaf5715
Truong <truongld@vinsofts.net>
2019-05-13 12:58:25 +0700

    fix ui

--- src/screens/CreateQRCodeScreen.js
+++ src/screens/CreateQRCodeScreen.js
@@ -10,7 +10,12 @@ export default class QRCodeScreen extends Component {
 		return (
 			<View style={styles.container} removeClippedSubviews={true}>
 				<Text style={styles.textStyle}>In mã này dán vào sách</Text>
-				
+				<TouchableOpacity
+					style={{ width: '100%', height: 50, justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue', marginTop: 10, }}
+					onPress={() => this.props.navigation.goBack()}
+				>
+					<Text style={{ fontSize: 16, fontWeight: '500', color: 'white' }}>IN MÃ QR</Text>
+				</TouchableOpacity>
 				<View
 					style={styles.qrCodeView}
 					overflow={'hidden'}
